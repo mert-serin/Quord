09b4278fc986721767570ed96cc383eb8ac43c52
Mert Serin <mert-serin@outlook.com>
2016-08-24 11:14:05 +0300

    first commit

---
+++ QuordTests/QuordTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuordTests.swift
+//  QuordTests
+//
+//  Created by Mert Serin on 24/08/16.
+//  Copyright © 2016 Mert Serin. All rights reserved.
+//
+
+import XCTest
+@testable import Quord
+
+class QuordTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
